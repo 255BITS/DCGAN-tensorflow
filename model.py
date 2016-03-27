[fix and tweak] remove scaling and tanh activation node

model.py
@@ -182,7 +182,7 @@ class DCGAN(object):
                     self.writer.add_summary(summary_str, counter)
 
                     # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-                    for repeat in range(3):
+                    for repeat in range(2):
                         # Update G network
                         _, summary_str = self.sess.run([g_optim, self.g_sum],
                             feed_dict={ self.z: batch_z })
@@ -198,9 +198,11 @@ class DCGAN(object):
                             time.time() - start_time, errD_fake+errD_real, errG))
 
                     SAVE_COUNT=10
-                    if np.mod(counter,SAVE_COUNT) == 2:
+                    if np.mod(counter, SAVE_COUNT) == 8:
+                        print("Saving after next batch")
+                    if np.mod(counter,SAVE_COUNT) == 9:
                         all_samples = []
-                        #bz = sample_z
+                        bz = sample_z
                         for i in range(1):
                             bz = np.random.normal(0.5, 0.5, [config.batch_size, self.z_dim]) \
                             #        .astype(np.float32)
@@ -274,11 +276,11 @@ class DCGAN(object):
             print('h3',h3.get_shape())
 
             h4, self.h4_w, self.h4_b = deconv2d(h3,
-                [self.batch_size, WAV_SIZE, WAV_HEIGHT, 2], name='g_h4', with_w=True)
+                [self.batch_size, WAV_SIZE, WAV_HEIGHT, 2], name='g_h4', stddev=4, with_w=True)
 
             print('h4',h4.get_shape())
             tanh = tf.nn.tanh(h4)
-            return tensorflow_wav.scale_up(tanh)
+            return tensorflow_wav.scale_up(h4)
 
     def sampler(self, z, y=None):
         tf.get_variable_scope().reuse_variables()
@@ -306,9 +308,9 @@ class DCGAN(object):
             h4 = deconv2d(h3, [self.batch_size, 64, 64, 2], name='g_h4')
             print('h4', h4.get_shape())
 
-            tanh = tf.nn.tanh(h4)
+            #tanh = tf.nn.tanh(h4)
 
-            return tensorflow_wav.scale_up(tanh)
+            return tensorflow_wav.scale_up(h4)
 
     def save(self, checkpoint_dir, step):
         model_name = "DCGAN.model"

ops.py
@@ -57,7 +57,7 @@ def conv_cond_concat(x, y):
     return tf.concat(3, [x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])])
 
 def conv2d(input_, output_dim, 
-           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.2,
            name="conv2d"):
     with tf.variable_scope(name):
         w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
@@ -70,7 +70,7 @@ def conv2d(input_, output_dim,
         return conv
 
 def deconv2d(input_, output_shape,
-             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.2,
              name="deconv2d", with_w=False):
     with tf.variable_scope(name):
         # filter : [height, width, output_channels, in_channels]

tensorflow_wav.py
@@ -167,23 +167,45 @@ def decode(input, bitrate=2048):
 
 def scale_up(input):
     #return input
-    #return input*(32768)
-    return (1/tf.exp(input*7.8*math.pi)) #compose(decompose(1/tf.exp(input*30)))
+    #return (((input-1)*2))*(32768/2)
+    #return (1/(input**3)-1)*32768
+    #input = (input-1)*2
+    #return (1/(i1-tf.exp(input**2))) #compose(decompose(1/tf.exp(input*30)))
     #scale_w2 = tf.get_variable("scale_w2", [1,1], initializer = tf.constant_initializer(1000))
     #scale_w2 = tf.get_variable("scale_w2", [1,1], dtype='complex64', initializer = tf.constant_initializer(1000000+1000j))
     #exp_scale = tf.get_variable("scale_exp", [1,1], initializer = tf.constant_initializer(1))
 
-    real, imag = tf.split(3, 2, input)
-    real = (1/tf.exp(real*3*math.pi)) #compose(decompose(1/tf.exp(input*30)))
-    return tf.concat(3, [real, tf.zeros_like(imag)])
+    #real, imag = tf.split(3, 2, input)
+    #real = (1/tf.exp(real*3*math.pi)) #compose(decompose(1/tf.exp(input*30)))
+    #return tf.concat(3, [real, tf.zeros_like(imag)])
     #return tf.sqrt(2.0)*input*w
     #return input * 10
+    #shape = [-1, 64*64*2]
+    #orig_shape = input.get_shape()
+    #input_dim = int(shape[1])
+    #W = tf.get_variable("scale_w", [input_dim, input_dim], initializer=tf.random_normal_initializer(0,stddev=8000))
+    #b = tf.get_variable("scale_b", [input_dim], initializer=tf.constant_initializer(100))
+    #output = tf.matmul(tf.reshape(input, shape),W) + b
+
+    #output = tf.nn.tanh(output)
+    #W = tf.get_variable("scale_w2", [256, input_dim], initializer=tf.random_normal_initializer(0,stddev=100))
+    #b = tf.get_variable("scale_b2", [input_dim], initializer=tf.constant_initializer(100))
+    #output = tf.matmul(output,W) + b
+    #output = tf.reshape(output, orig_shape)
+    #return output
+     #beeps
     real, imag = tf.split(3, 2, input)
-    #real_sign = tf.sign(real)
-    imag_sign = tf.sign(imag)
-    new_real = (1/tf.exp(real*8*math.pi))*real_sign
-    new_imag = (1/tf.exp(imag*8*math.pi))*imag_sign
+    #pi=math.pi
+    new_real = (real**2)*tf.sign(real)
+    new_imag = (imag**2)*tf.sign(imag)
+    #new_real = 1/(real**2)*tf.sign(real)
+    #new_imag = 1/(imag**2)*tf.sign(imag)
+    #new_real = tf.maximum(tf.minimum(32000., 1/tf.sqrt(real*pi)), -32000.)*tf.sign(real)
+    #new_imag = tf.maximum(tf.minimum(32000., 1/tf.sqrt(imag*pi)), -32000.)*tf.sign(imag)
     return tf.concat(3, [new_real, new_imag])
+
+    #return tf.maximum(1/tf.log(input)+0.5, -1)*16000
+
     #return (1/tf.exp(input*8*math.pi)) #compose(decompose(1/tf.exp(input*30)))
     #return input*100#*math.pi)) #compose(decompose(1/tf.exp(input*30)))
     #pow = 2